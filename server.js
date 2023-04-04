cfbd1c31329ed3b3ea8c65a4f7f8053d5e372f87
nicholas4418 <nicholas4418@gmail.com>
2023-04-03 21:35:26 -0400

    Added code to connection.js and server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+//Importing required packages
+const db = require('./config/connection');
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
